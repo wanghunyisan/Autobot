2c68dd686ed707957b98404490952cc74021b3a4
grimm <wanghunyisan@hotmail.com>
2018-07-12 17:08:12 +0800

    commit

--- src/main/java/com/grimm/maven/selenium/mantis/pageobject/HomePage.java
+++ src/main/java/com/grimm/maven/selenium/mantis/pageobject/HomePage.java
@@ -1,5 +1,21 @@
 package com.grimm.maven.selenium.mantis.pageobject;
 
-public class HomePage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+public class HomePage extends BasePage {
+
+	// 登录链接
+	@FindBy(xpath = "")
+	WebElement login_lnk;
+
+	public HomePage(WebDriver driver) {
+		super(driver);
+	}
+
+	public LoginPage clickLoginLnk() {
+		click(login_lnk);
+		return new LoginPage(driver);
+	}
 }
--- src/main/java/com/grimm/maven/selenium/mantis/tools/Login.java
+++ src/main/java/com/grimm/maven/selenium/mantis/tools/Login.java
@@ -3,7 +3,11 @@ package com.grimm.maven.selenium.mantis.tools;
 import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
 import com.grimm.maven.selenium.mantis.driver.BrowserEngine;
+import com.grimm.maven.selenium.mantis.pageobject.HomePage;
+import com.grimm.maven.selenium.mantis.pageobject.LoginPage;
 
 public class Login {
 	public WebDriver driver;
@@ -16,5 +20,11 @@ public class Login {
 	}
 
 	public void loginValid() {
+		HomePage homepge = PageFactory.initElements(driver, HomePage.class);
+		// 点击主页上的登录链接
+		homepge.clickLoginLnk();
+		// 跳转到登录页面，初始化登录界面的元素
+		LoginPage loginpage = PageFactory.initElements(driver, LoginPage.class);
+		loginpage.login("", "");
 	}
 }
