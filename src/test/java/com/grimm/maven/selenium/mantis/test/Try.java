f8153d6469a5c3d4b8faee991a5d20b713302749
grimm <wanghunyisan@hotmail.com>
2018-11-29 15:14:38 +0800

    commit

--- src/test/java/com/grimm/maven/selenium/mantis/test/Try.java
+++ src/test/java/com/grimm/maven/selenium/mantis/test/Try.java
@@ -19,7 +19,7 @@ public class Try {
 		driver = browserEngine.getBrowser();
 	}
 
-	@Test
+	@Test(invocationCount = 10, threadPoolSize = 5)//invocationCount：调用次数，threadPoolSize：线程数
 	public void getTitle() {
 		System.out.println(driver.getTitle());
 	}
